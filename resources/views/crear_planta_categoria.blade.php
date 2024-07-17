4b54dad1a3b8758693f348f867f745754bb776e0
notlifeboi <notlifeboi@gmail.com>
2024-07-17 03:44:12 -0500

    Corrección vista planta

--- resources/views/crear_planta_categoria.blade.php
+++ resources/views/crear_planta_categoria.blade.php
@@ -20,12 +20,12 @@
             <input type="text" class="form-control" name="caracteristicasp">
           </div>
           <div class="mb-3">
-            <label class="form-label">Datos Claves</label>
-            <input type="text" class="form-control" name="datos_clavesp">
-          </div>
-          <div class="mb-3">
             <label class="form-label">Descripcion</label>
             <input type="text" class="form-control" name="descripcionp">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Datos Claves</label>
+            <input type="text" class="form-control" name="datos_clavesp">
           </div>
           <div class="mb-3">
             <label class="form-label">Imagen</label>
