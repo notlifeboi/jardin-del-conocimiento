676a3231d9900c8d63e0a4c027ec6d2b14989936
notlifeboi <notlifeboi@gmail.com>
2024-07-17 04:50:49 -0500

    ojalá sirva

--- app/Models/plantas.php
+++ app/Models/plantas.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class plantas extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre', 'nombre_cientifico', 'caracteristicas', 'descripcion', 'datos_claves', 'id_categoria', 'img'
+    ];
 }
--- database/migrations/2024_05_16_232809_plantas.php
+++ database/migrations/2024_05_16_232809_plantas.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\plantas;
+
 return new class extends Migration
 {
        /**
@@ -27,6 +29,27 @@ return new class extends Migration
             $table->string('img');
             $table->timestamps();
         });
+
+        $columnas = ['nombre', 'nombre_cientifico', 'caracteristicas', 'descripcion', 'datos_claves', 'id_categoria', 'img'];
+
+        $plantas = [
+        [
+            'Acoro', 
+            'Acorus calamus', 
+            'Altura: 60-120 cm. Hojas: Verdes, largas, aromáticas, con bordes finos. Flores: Pequeñas, de color amarillo verdoso, agrupadas en espádices cilíndricos.', 
+            'El Acorus calamus, conocido comúnmente como acoro o cálamo aromático, es una planta perenne y herbácea con una altura que oscila entre 60 y 120 cm. Posee hojas largas y lineales en forma de espada, que desprenden un aroma distintivo cuando se aplastan. La planta produce pequeñas flores amarillas verdosas agrupadas en espádices cilíndricos que emergen de un tallo lateral.', 
+            'Hábitat: Margen de ríos, lagos y pantanos. Origen: Regiones de Europa, Asia y América del Norte. Usos: Utilizado tradicionalmente en medicina como tónico digestivo y sedante. También se usa en perfumería y como planta ornamental en jardines acuáticos.', 
+            1, 
+            'Acoro.jpg'
+        ],
+        ];
+
+        foreach ($plantas as $planta) {
+            
+            $datosPlanta = array_combine($columnas, $planta);
+    
+            Planta::insert($datosPlanta);
+        }
     }
 
     /**
