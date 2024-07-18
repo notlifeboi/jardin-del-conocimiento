c40a331de1cf5c154d0dc2eb5b44a55b4d6ae453
notlifeboi <notlifeboi@gmail.com>
2024-07-18 16:28:30 -0500

    Corrección home / carrusel corregido

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,12 @@
 @extends ('plantilla')
 @section('contenido')
-<div id="carouselExampleCaptions" style="width: 500px; height: 500px;" class="carousel slide mb-5">
+<style>
+  .carousel-img {
+  object-fit: cover;
+  height: 400px; 
+  }
+</style>
+<div id="carouselExampleCaptions" class="carousel slide mb-5">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
