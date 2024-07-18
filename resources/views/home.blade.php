be68c5d2223d91675d345babe92ab5d342ba307b
notlifeboi <notlifeboi@gmail.com>
2024-07-18 18:21:20 -0500

    qué hice

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,13 +37,10 @@
         <a href="/top3"><button style="font-family: 'italic';"class="btn btn-outline-success col-12 m-1">ver mas</button></a>
       </div>
       <div class="col col-lg-6 col-md-12 col-sm-12" id="col_home">
-        <img src="img\categorias\Carnivoras.jpg" class="img_home" alt="imagen3">
-      </div>
-    </div>
-    <div class="row">
-    <div class="col col-lg-6 col-md-12 col-sm-12" id="col_home">
       <img src="img/categorias/Carnivoras.jpg" class="img_home" alt="imagen3" style="width: 100%; height: 100%; object-fit: cover;">
     </div>
+    </div>
+    <div class="row">
         <div class="col col-lg-6 col-md-12 col-sm-12" id="col_home" >
             <p>Destacamos los beneficios tanto físicos como emocionales
                 que el contacto con la naturaleza y el cultivo de plantas
