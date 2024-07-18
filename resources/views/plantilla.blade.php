e1ef0df9aec7f08b1ff9aeddb045a6373f3d731c
notlifeboi <notlifeboi@gmail.com>
2024-07-18 15:54:21 -0500

    Corrección plantilla / buscar

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -186,7 +186,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class=""><i class="fa-solid fa-bars fa-xl" style="color: #ffffff;"></i></span> 
           </button>
-          <div class="collapse navbar-collapse col-6" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse mt-4 col-12" id="navbarSupportedContent">
           <div class="me-auto">
             <form action="{{ route('buscar') }}" method="GET" class="d-flex col-lg-12 col-md-12" role="search">
               <input class="form-control me-2" type="search" name="buscar" placeholder="Buscar" aria-label="Search">
