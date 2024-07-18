248b420b15612dedfa844b1db8cab6c0683d0327
notlifeboi <notlifeboi@gmail.com>
2024-07-18 18:01:36 -0500

    JUMMMMMMMMMMMM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,20 @@
 @extends ('plantilla')
 @section('contenido')
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="img\img1.jpg" alt="First slide">
+      <img class="d-block w-100" src="img/img1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="img\img2.jpg" alt="Second slide">
+      <img class="d-block w-100" src="img/img2.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="img\img3.jpg" alt="Third slide">
+      <img class="d-block w-100" src="img/img3.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
