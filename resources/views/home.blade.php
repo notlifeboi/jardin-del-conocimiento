9c0e9637e8c6bb9532eab6eb7288678aaa897ac9
notlifeboi <notlifeboi@gmail.com>
2024-07-18 16:59:22 -0500

    Nuevo carrusel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,26 @@
 @extends ('plantilla')
 @section('contenido')
-<div id="carouselExampleCaptions" class="carousel slide mb-5">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img\img1.jpg" alt="First slide">
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="img\img1.jpg" style="object-fit: cover;" class="d-block w-100" alt="imagen1">
-      </div>
-      <div class="carousel-item">
-        <img src="img\img2.jpg" style="object-fit: cover;" class="d-block w-100" alt="imagen2">
-      </div>
-      <div class="carousel-item">
-        <img src="img\img3.jpg" style="object-fit: cover;" class="d-block w-100" alt="imagen3"> 
-      </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img\img2.jpg" alt="Second slide">
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img\img3.jpg" alt="Third slide">
+    </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
   <div class="container">
     <div class="row my-5" id="top3">
       <div class="col col-lg-6 col-md-12 col-sm-12">
