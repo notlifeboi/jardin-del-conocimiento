44d376f0b80de8da174bfa2242084ae8ef796fb2
notlifeboi <notlifeboi@gmail.com>
2024-07-18 15:36:22 -0500

    Corrección plantilla / buscar

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -183,23 +183,23 @@
           <li>
           <a class="navbar-brand" href="/"><img style="width: 60px;height: 60px;" src={{ asset('img/logo.png') }} alt="logo"></a>
           </li>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class=""><i class="fa-solid fa-bars fa-xl" style="color: #ffffff;"></i></span> 
-          </button>
           <li>
             <form action="{{ route('buscar') }}" method="GET" class="d-flex col-lg-12 col-md-12 mt-1" role="search">
               <input class="form-control me-2" type="search" name="buscar" placeholder="Buscar" aria-label="Search">
               <button class="btn btn-search" type="submit">
                   <i class="bi bi-search"></i>
               </button>
-            </form>
+              </form>
           </li>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""><i class="fa-solid fa-bars fa-xl" style="color: #ffffff;"></i></span> 
+          </button>
           <div class="collapse navbar-collapse col-6" id="navbarSupportedContent">
             <ul class="navbar-nav mt-3 ms-auto mb-3 mb-lg-3">
               <li class="nav-item me-3">
                 <a class="nav-link" href="/categorias" id="text1">Categorias</a>
               </li>
-              <li>
+            <li>
               @auth
     <button class="mt-3 mb-3 me-5" id="mostrarBtn">Conversación</button>
     <div id="contenedorLateral">
