ca966a5d912177030a55acabb11fb543703a88a4
notlifeboi <notlifeboi@gmail.com>
2024-07-18 21:46:17 -0500

    Intento de arreglar el foro #5

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -185,7 +185,7 @@
               <li>
               @auth
     <button class="mt-3 mb-3 me-5" id="mostrarBtn">Conversación</button>
-    <div id="contenedorLateral" class="abierto col col-lg-6 col-md-7 col-12">
+    <div id="contenedorLateral" class="col col-lg-6 col-md-7 col-12">
       <div style="background-color: #356047; width: 100%">
         <button style="margin-right:10px; margin-top:5px; color: white; border: none; font-weight: bold; background: transparent;" id="cerrarBtn">X</button>
       </div>
