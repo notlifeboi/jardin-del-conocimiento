98d25d66411a297eb869c85f6f8700ade90f5ba8
notlifeboi <notlifeboi@gmail.com>
2024-07-18 21:52:11 -0500

    Intento de arreglar el foro #7

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -180,7 +180,7 @@
     <button class="mt-3 mb-3 me-5" id="mostrarBtn">Conversación</button>
     <div id="contenedorLateral" class="col col-lg-6 col-md-7 col-12">
       <div style="background-color: #356047; width: 100%">
-        <button style="margin-right:10px; margin-top:5px; color: white; border: none; font-weight: bold; background: transparent;" id="cerrarBtn">X</button>
+        <button style="margin-right:10px; margin-top:5px; color: white; border: none; font-weight: bold; background: transparent;" id="cerrarBtn">  < Cerrar</button>
       </div>
         <div id="listaComentarios" style="height: 80vh; overflow-y: auto">
         @foreach($comments as $comment)
